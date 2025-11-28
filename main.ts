a101f091191b692ef12d2dab82bb80fa46300515
Adam Orczyk <adam.orczyk@gmail.com>
2025-11-28 17:57:17 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,18 @@
 vcController.onVCsetup(function () {
-    bluetooth.uartWriteLine('vc;init;')
-    bluetooth.uartWriteLine('vc;sl;1;-100;100;1;1;0;1;;')
-    bluetooth.uartWriteLine('vc;sr;1;-60;60;1;1;0;0;;')
-    bluetooth.uartWriteLine('vc;jrx;-60;60;1;0;0;')
-    bluetooth.uartWriteLine('vc;jry;-100;100;1;0;0;')
-    bluetooth.uartWriteLine('vc;b;1;1;0;<i class="fa-regular fa-lightbulb"></i>;')
-    bluetooth.uartWriteLine('vc;b;2;0;0;2;')
-    bluetooth.uartWriteLine('vc;b;3;1;0;<i class="fa-solid fa-lightbulb"></i>;')
-    bluetooth.uartWriteLine('vc;b;4;0;0;4;')
-    bluetooth.uartWriteLine('vc;ox;1;-45;45;-60;60;1;1;0;')
-    bluetooth.uartWriteLine('vc;oy;0;-45;45;-7;7;1;0;0;')
-    bluetooth.uartWriteLine('vc;il;1;')
-    bluetooth.uartWriteLine('vc;ir;2;')
-    bluetooth.uartWriteLine('vc;show;sl,sr,jr,br,bl;')
-})
-bluetooth.onBluetoothDisconnected(function () {
-    wuKong.setLightMode(wuKong.LightMode.OFF)
-    strip.setPixelColor(0, 0)
-    strip.setPixelColor(1, 0)
-    strip.show()
+    bluetooth.uartWriteLine("vc;init;")
+    bluetooth.uartWriteLine("vc;sl;1;-100;100;1;1;0;1;;")
+    bluetooth.uartWriteLine("vc;sr;1;-60;60;1;1;0;0;;")
+    bluetooth.uartWriteLine("vc;jrx;-60;60;1;0;0;")
+    bluetooth.uartWriteLine("vc;jry;-100;100;1;0;0;")
+    bluetooth.uartWriteLine("vc;b;1;1;0;<i class=\"fa-solid fa-volume-high\"></i>;")
+    bluetooth.uartWriteLine("vc;b;2;1;2;<i class=\"fa-regular fa-lightbulb\"></i>;")
+    bluetooth.uartWriteLine("vc;b;3;1;0;<i class=\"fa-regular fa-lightbulb\"></i>;")
+    bluetooth.uartWriteLine("vc;b;4;1;0;<i class=\"fa-solid fa-lightbulb\"></i>;")
+    bluetooth.uartWriteLine("vc;ox;1;-45;45;-60;60;1;0;0;")
+    bluetooth.uartWriteLine("vc;oy;0;-45;45;-7;7;1;0;0;")
+    bluetooth.uartWriteLine("vc;il;1;")
+    bluetooth.uartWriteLine("vc;ir;2;")
+    bluetooth.uartWriteLine("vc;show;sl,sr,jr,br,bl;")
 })
 vcController.onVCcommand(function () {
     if (vcController.isSlider(InputSide.Right) || vcController.isOrientation(InputOrientaton.x) || vcController.isJoystick(InputSide.Right, JoystickDirection.x)) {
@@ -27,30 +21,40 @@ vcController.onVCcommand(function () {
     if (vcController.isSlider(InputSide.Left) || vcController.isJoystick(InputSide.Right, JoystickDirection.y)) {
         wuKong.setServoSpeed(wuKong.ServoList.S7, vcController.getCommandValue())
     }
-    if (vcController.isKey("1", KeyState.Pressed)) {
-        if (light2) {
+    if (vcController.isKey("3", KeyState.Pressed)) {
+        if (vcController.buttonToggled()) {
             wuKong.setLightMode(wuKong.LightMode.OFF)
-            light2 = 0
         } else {
             wuKong.setLightMode(wuKong.LightMode.BREATH)
-            light2 = 1
         }
     }
-    if (vcController.isKey("3", KeyState.Pressed)) {
-        if (light1) {
+    if (vcController.isKey("4", KeyState.Pressed)) {
+        if (vcController.buttonToggled()) {
             strip.setPixelColor(0, 0)
             strip.setPixelColor(1, 0)
-            light1 = 0
         } else {
             strip.setPixelColor(0, 1048575)
             strip.setPixelColor(1, 1048575)
-            light1 = 1
         }
         strip.show()
     }
+    if (vcController.isKey("2", KeyState.Pressed)) {
+        if (vcController.buttonToggled()) {
+            pins.analogWritePin(AnalogPin.P12, 1023)
+            pins.analogWritePin(AnalogPin.P15, 1023)
+        } else {
+            pins.analogWritePin(AnalogPin.P12, 0)
+            pins.analogWritePin(AnalogPin.P15, 0)
+        }
+    }
+    if (vcController.isKey("1", KeyState.Pressed)) {
+        music.ringTone(262)
+    }
+    if (vcController.isKey("1", KeyState.Released)) {
+        music.stopAllSounds()
+    }
 })
-let light1 = 0
-let light2 = 0
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 strip.setBrightness(50)
+music.setVolume(20)
